5af8d746bad692411f0850505f8ebb4902997ed6
junhee97 <rlawnsgml97@naver.com>
2025-06-02 17:49:10 +0900

    ownerpage

--- pet/src/main/java/com/example/demo/controller/mypage/MyPageController.java
+++ pet/src/main/java/com/example/demo/controller/mypage/MyPageController.java
@@ -1,7 +1,9 @@
 package com.example.demo.controller.mypage;
 
+import com.example.demo.domain.dto.OwnerDto;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,9 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Slf4j
 @RequestMapping("/mypage")
 public class MyPageController {
-    @GetMapping("/owner")
-    public String owner() {
-        return "mypage/owner";
+    @GetMapping("/ownerpage")
+    public String ownerPage(Model model) {
+        OwnerDto user = new OwnerDto("aaa", 2, 3, 1, 150000);
+        model.addAttribute("user", user);
+        return "mypage/ownerpage";
     }
 
     @GetMapping("/sitter")
