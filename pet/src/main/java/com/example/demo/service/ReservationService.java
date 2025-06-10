921895e850adf6d7056a62ea9605eca69763b806
jh-Kim\김준희 <rlawnsgml97@naver.com>
2025-06-11 00:30:05 +0900

    db 실시간 처리, 오너/시터 페이지

--- pet/src/main/java/com/example/demo/service/ReservationService.java
+++ pet/src/main/java/com/example/demo/service/ReservationService.java
@@ -4,4 +4,6 @@ import com.example.demo.domain.dto.ReservationRequestDto;
 
 public interface ReservationService {
     void makeReservation(ReservationRequestDto dto);
+
+    void updateReservationStatus();
 }
